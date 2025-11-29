f603cd87a00324667f1169ce7b09f31b29bd5de9
TheWhale01 <ard.rasp01@gmail.com>
2025-11-29 13:41:23 +0100

    updated paths

--- sys/secrets.nix
+++ sys/secrets.nix
@@ -2,45 +2,25 @@
 	age = {
 		secrets = {
 			transmission = {
-				file = ../../secrets/transmission.age;
+				file = ../secrets/transmission.age;
 				owner = "transmission";
 				group = "transmission";
 			};
 			traefikCfDnsToken= {
-				file = ../../secrets/traefik/cf_dns_token.age;
+				file = ../secrets/traefik/cf_dns_token.age;
 				owner = "traefik";
 				group = "traefik";
 			};
-			nextcloudAdminPass = {
-				file = ../../secrets/nextcloud.age;
-			};
-			litellm = {
-				file = ../../secrets/litellm.age;
-			};
-			homepageJellyfin = {
-				file = ../../secrets/homepage/jellyfin.age;
-			};
-			homepageJellyseerr = {
-				file = ../../secrets/homepage/jellyseerr.age;
-			};
-			homepageRadarr = {
-				file = ../../secrets/homepage/radarr.age;
-			};
-			homepageSonarr = {
-				file = ../../secrets/homepage/sonarr.age;
-			};
-			homepageBazarr = {
-				file = ../../secrets/homepage/bazarr.age;
-			};
-			homepageProwlarr = {
-				file = ../../secrets/homepage/prowlarr.age;
-			};
-			homepageTransmission = {
-				file = ../../secrets/homepage/transmission.age;
-			};
-			homepageNextcloud = {
-			  file = ../../secrets/homepage/nextcloud.age;
-			};
+			nextcloudAdminPass.file = ../secrets/nextcloud.age;
+			litellm.file = ../secrets/litellm.age;
+			homepageJellyfin.file = ../secrets/homepage/jellyfin.age;
+			homepageJellyseerr.file = ../secrets/homepage/jellyseerr.age;
+			homepageRadarr.file = ../secrets/homepage/radarr.age;
+			homepageSonarr.file = ../secrets/homepage/sonarr.age;
+			homepageBazarr.file = ../secrets/homepage/bazarr.age;
+			homepageProwlarr.file = ../secrets/homepage/prowlarr.age;
+			homepageTransmission.file = ../secrets/homepage/transmission.age;
+			homepageNextcloud.file = ../secrets/homepage/nextcloud.age;
 		};
 	};
 }
